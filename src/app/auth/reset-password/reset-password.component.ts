ceb7c3827f5bf3593b71bd28ae897adfa2a4234f
Mohamed Rezk <Mohamed.Rezk@scai.intra>
2021-03-24 09:23:54 +0100

    go back btn reset psw added

--- src/app/auth/reset-password/reset-password.component.ts
+++ src/app/auth/reset-password/reset-password.component.ts
@@ -1,23 +1,29 @@
 import { Component, OnInit, ViewChild } from "@angular/core";
 import { NgForm } from "@angular/forms";
-import { AuthLocalStorage } from "../auth-local-storage.service";
+import { Router } from "@angular/router";
 import { AuthService } from "../auth.service";
 
 @Component({
     selector: 'app-reset-password',
     templateUrl: './reset-password.component.html',
-    styleUrls: ['./reset-password.component.css']
+    styleUrls: ['./reset-password.component.scss']
 })
 export class ResetPasswordComponent implements OnInit {
     @ViewChild('f') resetForm: NgForm;
 
-    constructor(private authService: AuthService){}
+    constructor(private authService: AuthService, 
+                private router: Router){}
 
     ngOnInit(){}
 
     onSubmit(){
         console.log('rest started');
         console.log(this.resetForm.value.email);
+        //this.router.navigate(['']);
         this.authService.resetPassword(this.resetForm.value.email);
+    }
+
+    onNavigate(){
+        this.router.navigate(['']);
     }
 }
